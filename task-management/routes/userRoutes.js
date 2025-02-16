0cde0c72f96018d2f58c2828c255c670a2cb3e4a
Wael <hadded831@gmail.com>
2025-02-16 03:59:33 +0100

    update routes

--- task-management/routes/userRoutes.js
+++ task-management/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('./userController');
 
 // Register a new user
 router.post('/register', userController.register);
