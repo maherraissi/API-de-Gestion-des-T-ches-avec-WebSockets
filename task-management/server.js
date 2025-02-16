4f54ede612f64b796d3b63af9183da16ceeb1bdc
jilanigharbi <gharbijilani001@gmail.com>
2025-02-16 16:49:22 +0100

    Adding the index.html and fixing the WS

--- task-management/server.js
+++ task-management/server.js
@@ -7,22 +7,32 @@ const taskRouter = require('./routes/taskRoutes');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",  // Allow frontend clients to connect
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Attach io to the request object (so it can be used in controllers)
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
 app.use(taskRouter);
 
-
 app.get('/', (req, res) => {
-    res.send('Welcome to the Task Management API');
+    res.send('Welcome to the Task Management API with WebSockets');
 });
 
 io.on('connection', (socket) => {
     console.log('New client connected');
-    
+
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
